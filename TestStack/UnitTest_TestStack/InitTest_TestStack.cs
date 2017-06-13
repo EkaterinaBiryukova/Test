e3fe9eb5824948d353c6374793e58f87cdafa30d
Ekaterina <ekaterinabiryukova1@gmail.com>
2017-06-13 10:12:46 +0300

    9.06

--- TestStack/UnitTest_TestStack/InitTest_TestStack.cs
+++ TestStack/UnitTest_TestStack/InitTest_TestStack.cs
@@ -7,19 +7,25 @@ namespace UnitTest_TestStack
     [TestClass]
     public class InitTest_TestStack
     {
+        /// <summary>
+        /// Test that we have correct result while working with correct input
+        /// </summary>
         [TestMethod]
         public void TestCorrecString()
         {
             myStack tst = new myStack("5 5 +");
-            int expected = 10;
+            double expected = 10;
             Assert.AreEqual(expected, tst.myPop(), "test passed");
         }
+        /// <summary>
+        /// Test to return an exception type myException when the string is incorrect
+        /// </summary>
         [TestMethod]
+        [ExpectedException(typeof(myException))] //this one shows that an exception can be return while testing in such way 
         public void TestIncorrectString()
         {
             myStack tst = new myStack("5 +");
-            //Assert.
-            // exception returns
+
         }
     }
 }
